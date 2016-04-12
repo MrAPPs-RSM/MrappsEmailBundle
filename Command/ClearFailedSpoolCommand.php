b5cd9a42302837c408b0f3477832bc5faf6d11f3
Samuele Mazza <samuele@mr-apps.com>
2016-04-12 18:13:05 +0200

    aggiunto command per cancellazione email non inviate dopo vari tentativi

---
+++ Command/ClearFailedSpoolCommand.php
@@ -0,0 +1,51 @@
+<?php
+namespace Mrapps\EmailBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClearFailedSpoolCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('mrapps:email:clear-failures')
+            ->setDescription('Clears email failures from the spool');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $transport = $this->getContainer()->get('swiftmailer.transport.real');
+        if (!$transport->isStarted()) {
+            $transport->start();
+        }
+
+        $spoolPath = $this->getContainer()->getParameter('swiftmailer.spool.file.path');
+        $finder = Finder::create()->in($spoolPath)->name('*.sending');
+
+        foreach ($finder as $failedFile) {
+            // rename the file, so no other process tries to find it
+            $tmpFilename = $failedFile . '.finalretry';
+            rename($failedFile, $tmpFilename);
+
+            /** @var $message \Swift_Message */
+            $message = unserialize(file_get_contents($tmpFilename));
+            $output->writeln(sprintf(
+                'Retrying <info>%s</info> to <info>%s</info>',
+                $message->getSubject(),
+                implode(', ', array_keys($message->getTo()))
+            ));
+
+            try {
+                $transport->send($message);
+                $output->writeln('Sent!');
+            } catch (\Swift_TransportException $e) {
+                $output->writeln('<error>Send failed - deleting spooled message</error>');
+            }
+
+            // delete the file, either because it sent, or because it failed
+            unlink($tmpFilename);
+        }
+    }
+}
