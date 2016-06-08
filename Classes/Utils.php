98f61a0d93171cfc6890f247f85b89db0b6340f2
lucamussoni <musso88@gmail.com>
2016-06-08 15:12:38 +0200

    stringhe troncate

--- Classes/Utils.php
+++ Classes/Utils.php
@@ -28,4 +28,26 @@ class Utils
 
         $container->get("mailer")->send($message);
     }
+
+    public static function troncaStringa($stringa = '', $lengthNeeded = 100, $stripTags = true) {
+
+        $lengthNeeded = intval($lengthNeeded);
+        if(($lengthNeeded <= 0) && !$stripTags) return $stringa;
+
+        if($stripTags) $stringa = strip_tags($stringa);
+
+        $parts = preg_split('/([\s\n\r]+)/', $stringa, null, PREG_SPLIT_DELIM_CAPTURE);
+        $parts_count = count($parts);
+
+        $length = 0;
+        $last_part = 0;
+        for (; $last_part < $parts_count; ++$last_part) {
+            $length += strlen($parts[$last_part]);
+            if ($length > $lengthNeeded) {
+                break;
+            }
+        }
+
+        return implode(array_slice($parts, 0, $last_part))."...";
+    }
 }
---
+++ Twig/MrappsEmailTwigExtension.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mrapps\EmailBundle\Twig;
+
+use Twig_Extension;
+use Mrapps\EmailBundle\Classes\Utils;
+
+class MrappsEmailTwigExtension extends Twig_Extension {
+    
+    private $container;
+
+    public function __construct($container) {
+        $this->container = $container;
+    }
+
+    /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return array An array of functions
+     */
+    public function getFunctions() {
+        return array();
+    }
+    
+    public function getFilters() {
+        return array(
+            new \Twig_SimpleFilter('tronca', function($string, $length) {
+                return Utils::troncaStringa($string, $length, false);
+            }),
+        );
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName() {
+        return 'mrapps_email_twig_extension';
+    }
+}
